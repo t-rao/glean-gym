f3a010c4b8b1ed9f08b445b5b091f6137b252245
Trivikram244 <trivikram244@gmail.com>
2018-05-26 21:14:59 +0530

    mobile_done

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("gmap"),mapProp);
+}
